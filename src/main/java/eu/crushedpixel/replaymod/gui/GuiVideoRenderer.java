8420e98ba65dcf920cd71ef074b9c226d64e68d8
CrushedPixel <crushedpixelmaps@gmail.com>
2015-05-24 02:01:35 +0200

    Localized GuiVideoRenderer

--- src/main/java/eu/crushedpixel/replaymod/gui/GuiVideoRenderer.java
+++ src/main/java/eu/crushedpixel/replaymod/gui/GuiVideoRenderer.java
@@ -4,6 +4,7 @@ import eu.crushedpixel.replaymod.video.VideoRenderer;
 import eu.crushedpixel.replaymod.video.frame.FrameRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.client.config.GuiCheckBox;
 
 import java.io.IOException;
@@ -11,10 +12,11 @@ import java.io.IOException;
 public class GuiVideoRenderer extends GuiScreen {
     private final VideoRenderer renderer;
 
-    private final String PAUSE_RENDERING = "Pause Rendering";
-    private final String RESUME_RENDERING = "Resume Rendering";
-    private final String CANCEL = "Cancel Rendering";
-    private final String CANCEL_CONFIRM = "Sure?";
+    private final String PAUSE_RENDERING = I18n.format("replaymod.gui.rendering.pause");
+    private final String RESUME_RENDERING = I18n.format("replaymod.gui.rendering.resume");
+    private final String CANCEL = I18n.format("replaymod.gui.rendering.cancel");
+    private final String CANCEL_CONFIRM = I18n.format("replaymod.gui.rendering.cancel.callback");
+    private final String PREVIEW = I18n.format("replaymod.gui.rendering.preview");
 
     private GuiButton pauseButton;
     private GuiButton cancelButton;
@@ -29,15 +31,17 @@ public class GuiVideoRenderer extends GuiScreen {
     public void initGui() {
         FrameRenderer frameRenderer = renderer.getFrameRenderer();
 
-        String text = "Show Preview (Performance might suffer)";
+        String text = PREVIEW;
         buttonList.add(previewCheckBox = new GuiCheckBox(0, (width - fontRendererObj.getStringWidth(text)) / 2 - 8,
                 frameRenderer.getPreviewHeight(this), text, false));
 
         text = PAUSE_RENDERING;
-        buttonList.add(pauseButton = new GuiButton(1, width / 2 - 202, height / 2 - 23, text));
+        buttonList.add(pauseButton = new GuiButton(1, width / 2 - 152, height / 2 - 23, text));
 
         text = CANCEL;
         buttonList.add(cancelButton = new GuiButton(1, width / 2 + 2, height / 2 - 23, text));
+
+        pauseButton.width = cancelButton.width = 150;
     }
 
     @Override
@@ -81,7 +85,7 @@ public class GuiVideoRenderer extends GuiScreen {
 
         drawBackground(0);
 
-        String framesProgress = String.format("Frames rendered: %d / %d", renderer.getFramesDone(), renderer.getTotalFrames());
+        String framesProgress = I18n.format("replaymod.gui.rendering.progress", renderer.getFramesDone(), renderer.getTotalFrames());
         drawCenteredString(fontRendererObj, framesProgress, centerX, centerY - 45, 0xffffffff);
 
         if (previewCheckBox.isChecked()) {
