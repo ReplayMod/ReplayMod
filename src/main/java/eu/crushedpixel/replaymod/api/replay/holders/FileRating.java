e446a0cc6b9842923d6b88e172cb4603a4df12ec
CrushedPixel <crushedpixelmaps@gmail.com>
2015-06-29 16:02:34 +0200

    Updated FileRating Holder class to fit updated get_ratings API call which now returns a boolean instead of numeric string

--- src/main/java/eu/crushedpixel/replaymod/api/replay/holders/FileRating.java
+++ src/main/java/eu/crushedpixel/replaymod/api/replay/holders/FileRating.java
@@ -3,13 +3,13 @@ package eu.crushedpixel.replaymod.api.replay.holders;
 public class FileRating {
 
     private int file;
-    private String rating;
+    private boolean rating;
 
     public int getFileID() {
         return file;
     }
 
     public boolean getRating() {
-        return rating.equals("1");
+        return rating;
     }
 }
