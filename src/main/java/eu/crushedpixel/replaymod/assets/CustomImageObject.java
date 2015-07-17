83b91b7d8ef53f90e6d3539d524ad17863d8d0da
CrushedPixel <crushedpixelmaps@gmail.com>
2015-07-17 13:33:48 +0200

    CustomImageObject#getResourceLocation() should return null if it's not registered yet (thread safety)

--- src/main/java/eu/crushedpixel/replaymod/assets/CustomImageObject.java
+++ src/main/java/eu/crushedpixel/replaymod/assets/CustomImageObject.java
@@ -88,6 +88,7 @@ public class CustomImageObject implements GuiEntryListEntry {
                     e.printStackTrace();
                 }
             }
+            return null;
         }
 
         if(resourceLocation != null && !ResourceHelper.isRegistered(resourceLocation)) {
