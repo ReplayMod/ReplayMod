931a58a6a9abcaf77fa29adc6cdcfe7a95983265
CrushedPixel <crushedpixelmaps@gmail.com>
2015-07-21 03:40:38 +0200

    Learn english, Johni!

--- src/main/java/eu/crushedpixel/replaymod/utils/Api.java
+++ src/main/java/eu/crushedpixel/replaymod/utils/Api.java
@@ -1,7 +1,7 @@
 package eu.crushedpixel.replaymod.utils;
 
 /**
- * Members annotated with @Api are considered API and should not be removed even if their unused as they might
+ * Members annotated with @Api are considered API and should not be removed even if they're unused as they might
  * be of use at a later point in time.
  * The purpose of this annotation is to be added to the IntelliJ inspections exceptions.
  */
