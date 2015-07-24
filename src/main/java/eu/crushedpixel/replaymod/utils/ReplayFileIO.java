4f94e41b9ab863b5a843b2fcc52d04967e7d913c
CrushedPixel <crushedpixelmaps@gmail.com>
2015-07-24 13:26:30 +0200

    Do not use Java 1.7 only File#toPath() method in ReplayFileIO#makeFolderFromPath

--- src/main/java/eu/crushedpixel/replaymod/utils/ReplayFileIO.java
+++ src/main/java/eu/crushedpixel/replaymod/utils/ReplayFileIO.java
@@ -18,6 +18,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
+import java.nio.file.FileSystems;
 import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -42,7 +43,7 @@ public class ReplayFileIO {
     }
 
     private static File makeFolderFromPath(String path) throws IOException {
-        File folder = new File(new File(path).toPath().normalize().toUri());
+        File folder = new File(FileSystems.getDefault().getPath(path).normalize().toUri());
         FileUtils.forceMkdir(folder);
         return folder;
     }
