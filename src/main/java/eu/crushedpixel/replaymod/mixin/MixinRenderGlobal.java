1d61d6c85c0b9b29a4bf0fa217beaee854289e9b
CrushedPixel <crushedpixelmaps@gmail.com>
2015-08-15 14:37:14 +0200

    Correctly pause/slow down/speed up the World Border Animation using Mixin magic

--- src/main/java/eu/crushedpixel/replaymod/mixin/MixinRenderGlobal.java
+++ src/main/java/eu/crushedpixel/replaymod/mixin/MixinRenderGlobal.java
@@ -1,6 +1,7 @@
 package eu.crushedpixel.replaymod.mixin;
 
 import eu.crushedpixel.replaymod.recording.ConnectionEventHandler;
+import eu.crushedpixel.replaymod.timer.EnchantmentTimer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.player.EntityPlayer;
@@ -9,6 +10,7 @@ import net.minecraft.util.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderGlobal.class)
@@ -23,4 +25,8 @@ public abstract class MixinRenderGlobal {
         }
     }
 
+    @Redirect(method = "renderWorldBorder", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getSystemTime()J"))
+    private long getEnchantmentTime() {
+        return EnchantmentTimer.getEnchantmentTime();
+    }
 }
