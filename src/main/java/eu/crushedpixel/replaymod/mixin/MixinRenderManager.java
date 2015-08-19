c6a471985002324ca22cb961c992c8fd07afb8d2
johni0702 <me@johni0702.de>
2015-08-19 14:24:19 +0200

    Fix nametag rotation when spectating from animals (vanilla bug)

--- src/main/java/eu/crushedpixel/replaymod/mixin/MixinRenderManager.java
+++ src/main/java/eu/crushedpixel/replaymod/mixin/MixinRenderManager.java
@@ -3,12 +3,17 @@ package eu.crushedpixel.replaymod.mixin;
 import eu.crushedpixel.replaymod.video.EntityRendererHandler;
 import eu.crushedpixel.replaymod.video.capturer.CubicOpenGlFrameCapturer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderManager.class)
@@ -27,6 +32,14 @@ public class MixinRenderManager {
             double yaw = -Math.atan2(dx, dz);
             playerViewX = (float) Math.toDegrees(pitch);
             playerViewY = (float) Math.toDegrees(yaw);
+        }
+    }
+
+    @Inject(method = "cacheActiveRenderInfo", at = @At("RETURN"))
+    public void fixHeadRotationForAnimals(World world, FontRenderer font, Entity view, Entity target, GameSettings settings, float partialRenderTick, CallbackInfo ci) {
+        if (view instanceof EntityAnimal && !((EntityAnimal) view).isPlayerSleeping()) {
+            EntityAnimal e = (EntityAnimal) view;
+            this.playerViewY = e.prevRotationYawHead + (e.rotationYawHead - e.prevRotationYawHead) * partialRenderTick;
         }
     }
 }
