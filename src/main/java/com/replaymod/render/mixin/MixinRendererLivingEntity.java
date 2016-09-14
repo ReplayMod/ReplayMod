d8e9f621bdc3fa02da8d3ca8016dd13fc07077c1
johni0702 <me@johni0702.de>
2016-09-14 20:04:22 +0200

    Completely hide invisible entities during replay Fixes invisible entities when in spectator mode during recording

--- src/main/java/com/replaymod/render/mixin/MixinRendererLivingEntity.java
+++ src/main/java/com/replaymod/render/mixin/MixinRendererLivingEntity.java
@@ -4,11 +4,9 @@ import com.replaymod.render.hooks.EntityRendererHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RendererLivingEntity.class)
@@ -19,20 +17,5 @@ public abstract class MixinRendererLivingEntity {
         if (handler != null && !handler.getSettings().isRenderNameTags()) {
             ci.setReturnValue(false); //this calls the cancel method
         }
-
-        // TODO
-//        if(ReplayHandler.isInReplay() && entity.isInvisible()
-//                && ReplaySettings.ReplayOptions.renderInvisible.getValue() == Boolean.FALSE) {
-//            ci.setReturnValue(false);
-//        }
-    }
-
-    @Redirect(method = "renderModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;isInvisibleToPlayer(Lnet/minecraft/entity/player/EntityPlayer;)Z"))
-    private boolean replayModRender_shouldInvisibleNotBeRendered(EntityLivingBase entity, EntityPlayer thePlayer) {
-        // TODO
-//        if(ReplaySettings.ReplayOptions.renderInvisible.getValue() == Boolean.TRUE|| !ReplayHandler.isInReplay()) {
-//            return entity.isInvisibleToPlayer(thePlayer);
-//        }
-        return true; //the original method inverts the return value
     }
 }
---
+++ src/main/java/com/replaymod/replay/mixin/MixinRendererLivingEntity.java
@@ -0,0 +1,28 @@
+package com.replaymod.replay.mixin;
+
+import com.replaymod.replay.camera.CameraEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RendererLivingEntity.class)
+public abstract class MixinRendererLivingEntity {
+    @Inject(method = "canRenderName", at = @At("HEAD"), cancellable = true)
+    private void replayModReplay_canRenderInvisibleName(EntityLivingBase entity, CallbackInfoReturnable<Boolean> ci) {
+        EntityPlayer thePlayer = Minecraft.getMinecraft().thePlayer;
+        if (thePlayer instanceof CameraEntity && entity.isInvisible()) {
+            ci.setReturnValue(false);
+        }
+    }
+
+    @Redirect(method = "renderModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;isInvisibleToPlayer(Lnet/minecraft/entity/player/EntityPlayer;)Z"))
+    private boolean replayModReplay_shouldInvisibleNotBeRendered(EntityLivingBase entity, EntityPlayer thePlayer) {
+        return thePlayer instanceof CameraEntity || entity.isInvisibleToPlayer(thePlayer);
+    }
+}
