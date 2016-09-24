8e6eec879f149d7bc612c0abc854d8e01e90db63
johni0702 <me@johni0702.de>
2016-09-24 17:11:25 +0200

    Fix name tag hiding during redering (fixes #10)

--- src/main/java/com/replaymod/render/mixin/MixinRender.java
+++ src/main/java/com/replaymod/render/mixin/MixinRender.java
@@ -7,15 +7,14 @@ import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Render.class)
 public abstract class MixinRender {
-    @Inject(method = "canRenderName", at = @At("HEAD"), cancellable = true)
-    private void replayModRender_areAllNamesHidden(Entity entity, CallbackInfoReturnable<Boolean> ci) {
+    @Inject(method = "renderLivingLabel", at = @At("HEAD"), cancellable = true)
+    private void replayModRender_areAllNamesHidden(Entity entityIn, String str, double x, double y, double z, int maxDistance, CallbackInfo ci) {
         EntityRendererHandler handler = ((EntityRendererHandler.IEntityRenderer) Minecraft.getMinecraft().entityRenderer).replayModRender_getHandler();
         if (handler != null && !handler.getSettings().isRenderNameTags()) {
-            ci.setReturnValue(false);
             ci.cancel();
         }
     }
