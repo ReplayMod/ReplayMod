9ff767b054066dc08b8cb434f6afc8ce50136503
johni0702 <me@johni0702.de>
2016-11-09 10:23:58 +0100

    Fix spectated player having the arm model of the camera player

--- src/main/java/com/replaymod/replay/camera/CameraEntity.java
+++ src/main/java/com/replaymod/replay/camera/CameraEntity.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.settings.KeyBinding;
@@ -252,6 +253,15 @@ public class CameraEntity extends EntityPlayerSP {
             return Utils.getResourceLocationForPlayerUUID(view.getUniqueID());
         }
         return super.getLocationSkin();
+    }
+
+    @Override
+    public String getSkinType() {
+        Entity view = mc.getRenderViewEntity();
+        if (view != this && view instanceof AbstractClientPlayer) {
+            return ((AbstractClientPlayer) view).getSkinType();
+        }
+        return super.getSkinType();
     }
 
     @Override
