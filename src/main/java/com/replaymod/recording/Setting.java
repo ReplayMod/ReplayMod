84385ee371a5e7f9499ec9354af08fda61c4db5d
Brandon Houghton <bhoughton@cmu.edu>
2018-06-12 15:07:58 -0400

    Removed vscode folder Changed defaults

--- src/main/java/com/replaymod/recording/Setting.java
+++ src/main/java/com/replaymod/recording/Setting.java
@@ -5,7 +5,7 @@ import com.replaymod.core.SettingsRegistry;
 public final class Setting<T> extends SettingsRegistry.SettingKeys<T> {
     public static final Setting<Boolean> RECORD_SINGLEPLAYER = make("recordSingleplayer", "recordsingleplayer", true);
     public static final Setting<Boolean> RECORD_SERVER = make("recordServer", "recordserver", true);
-    public static final Setting<Boolean> INDICATOR = make("indicator", "indicator", true);
+    public static final Setting<Boolean> INDICATOR = make("indicator", "indicator", false);
 
     private static <T> Setting<T> make(String key, String displayName, T defaultValue) {
         return new Setting<>(key, displayName, defaultValue);
