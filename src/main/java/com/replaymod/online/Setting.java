e001bd874435ee8707b66034455c193ccfe6e3e3
Brandon Houghton <bhoughton@cmu.edu>
2018-06-12 16:11:11 -0400

    Enabled parallel builds Skip login propt by default

--- src/main/java/com/replaymod/online/Setting.java
+++ src/main/java/com/replaymod/online/Setting.java
@@ -3,7 +3,7 @@ package com.replaymod.online;
 import com.replaymod.core.SettingsRegistry;
 
 public final class Setting<T> extends SettingsRegistry.SettingKeys<T> {
-    public static final Setting<Boolean> SKIP_LOGIN_PROMPT = make("skipLoginPrompt", null, false);
+    public static final Setting<Boolean> SKIP_LOGIN_PROMPT = make("skipLoginPrompt", null, true);
     public static final SettingsRegistry.SettingKey<String> DOWNLOAD_PATH =
             new SettingsRegistry.SettingKeys<>("advanced", "downloadPath", null, "./replay_downloads/");
 
