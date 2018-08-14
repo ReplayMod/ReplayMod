a0cc966675042c8ae413e47237045855d36f678d
Brandon Houghton <bhoughton@cmu.edu>
2018-08-14 16:45:40 -0400

    Json string was rarely none - wait for non-null response

--- src/main/java/com/replaymod/recording/handler/ConnectionEventHandler.java
+++ src/main/java/com/replaymod/recording/handler/ConnectionEventHandler.java
@@ -424,18 +424,6 @@ public class ConnectionEventHandler {
         
         logger.info(String.format("Minecraft Key:    %s%n", minecraftKey));
 
-        // Clear previous authenication responses
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(mcServerSocket.getInputStream()));
-            while(in.ready()){
-                in.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            mcServerOut.close();
-            logger.error("Not able to flush input prior to mc server authentication");
-        }
-
         // Send key to Minecraft Server
         JsonObject authJson = new JsonObject();
         authJson.addProperty("cmd", "authorize_user");
@@ -455,7 +443,9 @@ public class ConnectionEventHandler {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(mcServerSocket.getInputStream()));
             String jsonStr = "";
-            jsonStr = in.readLine();
+            while (jsonStr == null){
+                jsonStr = in.readLine();
+            }
             JsonObject authResponse = new JsonParser().parse(jsonStr).getAsJsonObject();
             authenicated = authResponse.get("authorized").getAsBoolean();
         } catch (JsonSyntaxException | IOException e) {
