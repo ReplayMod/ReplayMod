de0f40f4dd9a3dc950e890bf807c31979f4479e0
Jonas Herzig <me@johni0702.de>
2019-06-20 18:43:19 +0200

    [1.14] Allow opening of replay settings via ModMenu

---
+++ src/main/java/com/replaymod/core/gui/ModMenuApiImpl.java
@@ -0,0 +1,21 @@
+//#if MC>=11400
+package com.replaymod.core.gui;
+
+import com.replaymod.core.ReplayMod;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
+
+import java.util.function.Function;
+
+public class ModMenuApiImpl implements ModMenuApi {
+    @Override
+    public String getModId() {
+        return ReplayMod.MOD_ID;
+    }
+
+    @Override
+    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+        return parent -> new GuiReplaySettings(parent, ReplayMod.instance.getSettingsRegistry()).toMinecraft();
+    }
+}
+//#endif
