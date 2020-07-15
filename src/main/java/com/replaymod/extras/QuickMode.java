a94f4178223f46fd1dec9d28e9d06637536cc791
LindaJuffermans <github.com@juffermans.net>
2020-07-15 21:07:30 +0200

    Fixed for 1.15.2

--- src/main/java/com/replaymod/extras/QuickMode.java
+++ src/main/java/com/replaymod/extras/QuickMode.java
@@ -15,51 +15,55 @@ import de.johni0702.minecraft.gui.utils.EventRegistrations;
 import net.minecraft.client.MinecraftClient;
 
 public class QuickMode extends EventRegistrations implements Extra {
-  private ReplayModReplay module;
+    private ReplayModReplay module;
 
-  private final IGuiImage indicator = new GuiImage().setTexture(ReplayMod.TEXTURE, 40, 100, 16, 16).setSize(16, 16);
+    private final IGuiImage indicator = new GuiImage().setTexture(ReplayMod.TEXTURE, 40, 100, 16, 16).setSize(16, 16);
 
-  private MinecraftClient mc;
-  private boolean active;
-  //#if MC>=11400
-  private double originalGamma;
-  //#else
-  //$$ private float originalGamma;
-  //#endif
+    private MinecraftClient mc;
+    private boolean active;
+    //#if MC>=11400
+    private double originalGamma;
+    //#else
+    //$$ private float originalGamma;
+    //#endif
 
-  @Override
-  public void register(final ReplayMod mod) throws Exception {
-    this.module = ReplayModReplay.instance;
-    this.mc = mod.getMinecraft();
+    @Override
+    public void register(final ReplayMod mod) throws Exception {
+        this.module = ReplayModReplay.instance;
+        this.mc = mod.getMinecraft();
 
-    mod.getKeyBindingRegistry().registerKeyBinding("replaymod.input.quickmode", Keyboard.KEY_Q, new Runnable() {
-      @Override
-      public void run() {
-        active = !active;
-        ReplayHandler replayHandler = module.getReplayHandler();
-        if (replayHandler == null) return;
-        updateIndicator(replayHandler.getOverlay());
-        replayHandler.getReplaySender().setSyncModeAndWait();
-        mod.runLater(() ->
-          replayHandler.ensureQuickModeInitialized(() -> {
-            replayHandler.setQuickMode(active);
-            replayHandler.getReplaySender().setAsyncMode(true);
-          })
-        );
-      }
-    });
+        mod.getKeyBindingRegistry().registerKeyBinding("replaymod.input.quickmode", Keyboard.KEY_Q, new Runnable() {
+            @Override
+            public void run() {
+                ReplayHandler replayHandler = module.getReplayHandler();
+                if (replayHandler == null) {
+                    return;
+                }
+                active = !active;
+                updateIndicator(replayHandler.getOverlay());
+                replayHandler.getReplaySender().setSyncModeAndWait();
+                mod.runLater(() ->
+                        replayHandler.ensureQuickModeInitialized(() -> {
+                            replayHandler.setQuickMode(active);
+                            replayHandler.getReplaySender().setAsyncMode(true);
+                        })
+                );
+            }
+        });
 
-    register();
-  }
-
-  { on(ReplayOpenedCallback.EVENT, replayHandler -> updateIndicator(replayHandler.getOverlay())); }
-  private void updateIndicator(GuiReplayOverlay overlay) {
-    System.out.println("QuickMode::updateIndicator; active=" + (active ? "T" : "F"));
-    if (active) {
-      overlay.statusIndicatorPanel.addElements(new HorizontalLayout.Data(2), indicator);
-    } else {
-      overlay.statusIndicatorPanel.removeElement(indicator);
+        register();
     }
-  }
 
+    {
+        on(ReplayOpenedCallback.EVENT, replayHandler -> updateIndicator(replayHandler.getOverlay()));
+    }
+
+    private void updateIndicator(GuiReplayOverlay overlay) {
+        System.out.println("QuickMode::updateIndicator; active=" + (active ? "T" : "F"));
+        if (active) {
+            overlay.statusIndicatorPanel.addElements(new HorizontalLayout.Data(2), indicator);
+        } else {
+            overlay.statusIndicatorPanel.removeElement(indicator);
+        }
+    }
 }
