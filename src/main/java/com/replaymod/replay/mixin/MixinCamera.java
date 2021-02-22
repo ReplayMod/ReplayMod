01d47279fdea15a706cbbd3753847026cde491bf
Jonas Herzig <me@johni0702.de>
2021-02-23 00:59:56 +0100

    Split off 1.12.2 overwrite from MixinCamera

--- src/main/java/com/replaymod/replay/mixin/MixinCamera.java
+++ src/main/java/com/replaymod/replay/mixin/MixinCamera.java
@@ -1,31 +1,20 @@
-//#if MC>=11400
 package com.replaymod.replay.mixin;
 
 import com.replaymod.replay.camera.CameraEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//#if MC>=11500
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.util.math.Vector3f;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Shadow;
-//#else
-//$$ import com.mojang.blaze3d.platform.GlStateManager;
-//$$ import net.minecraft.world.BlockView;
-//#endif
-
-//#if MC>=11500
-@Mixin(net.minecraft.client.render.GameRenderer.class)
-//#else
-//$$ @Mixin(net.minecraft.client.render.Camera.class)
-//#endif
+@Mixin(GameRenderer.class)
 public class MixinCamera {
-    //#if MC>=11500
     @Shadow @Final private MinecraftClient client;
     @Inject(
             method = "renderWorld",
@@ -40,20 +29,4 @@ public class MixinCamera {
             matrixStack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(((CameraEntity) entity).roll));
         }
     }
-    //#else
-    //$$ @Inject(
-    //$$         method = "update",
-    //$$         at = @At(
-    //$$                 value = "INVOKE",
-    //$$                 target = "Lcom/mojang/blaze3d/platform/GlStateManager;rotatef(FFFF)V",
-    //$$                 ordinal = 0
-    //$$         )
-    //$$ )
-    //$$ private void applyRoll(BlockView blockView, Entity view, boolean boolean_1, boolean boolean_2, float float_1, CallbackInfo ci) {
-    //$$     if (view instanceof CameraEntity) {
-    //$$         GlStateManager.rotatef(((CameraEntity) view).roll, 0.0F, 0.0F, 1.0F);
-    //$$     }
-    //$$ }
-    //#endif
 }
-//#endif
