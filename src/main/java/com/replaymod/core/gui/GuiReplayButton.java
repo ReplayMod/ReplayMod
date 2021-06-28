2be47e98109a0b28f114d6d72ce2ab8a7142a075
Jonas Herzig <me@johni0702.de>
2021-06-28 17:47:15 +0200

    Change ReplayButton icon to be white with shadow for better contrast
    The previous one, blue with no shadow, had horrible contrast with the button
    background and did not fit well with any of the other buttons.
    Now it does (except that it's not yet in the 20-pixel width/height style).

--- src/main/java/com/replaymod/core/gui/GuiReplayButton.java
+++ src/main/java/com/replaymod/core/gui/GuiReplayButton.java
@@ -1,17 +1,19 @@
 package com.replaymod.core.gui;
 
-import com.replaymod.core.ReplayMod;
 import de.johni0702.minecraft.gui.GuiRenderer;
 import de.johni0702.minecraft.gui.RenderInfo;
 import de.johni0702.minecraft.gui.element.GuiButton;
 import de.johni0702.minecraft.gui.utils.lwjgl.ReadableDimension;
+import net.minecraft.util.Identifier;
 
 public class GuiReplayButton extends GuiButton {
+    public static final Identifier ICON = new Identifier("replaymod", "logo_button.png");
+
     @Override
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         super.draw(renderer, size, renderInfo);
 
-        renderer.bindTexture(ReplayMod.LOGO_FAVICON);
+        renderer.bindTexture(ICON);
         renderer.drawTexturedRect(3, 3, 0, 0, size.getWidth() - 6, size.getHeight() - 6, 1, 1, 1, 1);
     }
 }
