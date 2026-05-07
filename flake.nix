ce02a44c5cc3263668220f888106a5876c0b667d
Sim-hu <gisa0142@gmail.com>
2026-05-07 07:36:17 +0000

    build: use available Java 16 package

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             jdk8
-            jdk16
+            adoptopenjdk-hotspot-bin-16
             jdk17
             jdk21
             gradle
@@ -28,7 +28,7 @@
           shellHook = ''
             export JAVA_HOME=${pkgs.jdk21}
             export PATH=$JAVA_HOME/bin:$PATH
-            export GRADLE_OPTS="-Dorg.gradle.java.installations.paths=${pkgs.jdk8},${pkgs.jdk16},${pkgs.jdk17},${pkgs.jdk21} ''${GRADLE_OPTS:-}"
+            export GRADLE_OPTS="-Dorg.gradle.java.installations.paths=${pkgs.jdk8},${pkgs.adoptopenjdk-hotspot-bin-16},${pkgs.jdk17},${pkgs.jdk21} ''${GRADLE_OPTS:-}"
             echo "ReplayMod dev shell - Java: $(java -version 2>&1 | head -1)"
             echo "CPUs: $(nproc)  |  DRI: $(ls /dev/dri/ 2>/dev/null | tr '\n' ' ')"
           '';
