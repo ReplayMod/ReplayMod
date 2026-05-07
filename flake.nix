350b3b0227a839fc416f21b7b4b7d1d07c06abd8
Sim-hu <gisa0142@gmail.com>
2026-05-07 07:04:04 +0000

    build: add Nix dev shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "ReplayMod 1.19.4 dev environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            jdk17
+            gradle
+            ffmpeg-full
+            vulkan-tools
+            mesa
+            libGL
+            libGLU
+            pkg-config
+            git
+          ];
+
+          shellHook = ''
+            export JAVA_HOME=${pkgs.jdk17}
+            export PATH=$JAVA_HOME/bin:$PATH
+            echo "ReplayMod dev shell - Java: $(java -version 2>&1 | head -1)"
+            echo "CPUs: $(nproc)  |  DRI: $(ls /dev/dri/ 2>/dev/null | tr '\n' ' ')"
+          '';
+        };
+      });
+}
